30a693b74899f2dd0c12f203e5902e47c73a33cc
Hardik Bansal <hardikbansal24@gmail.com>
2017-12-24 19:11:50 +0530

    Embedding working

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import numpy
+import numpy as np
 import os
 import sys
 import pandas as pd
 import tensorflow as tf
-
-
+from options import trainOptions
 
 
 class DD():
 
-	def __initialize__(self):
+	def initialize(self):
 		
 		opt = trainOptions().parse()[0]
 
@@ -18,10 +17,10 @@ class DD():
 		self.to_test = opt.test
 		self.load_checkpoint = False
 		self.emb_size = opt.emb_size
+		self.num_token = opt.num_token
 		
-		self.tensorboard_dir = "./output/" + self.model + "/" + self.dataset + "/tensorboard"
-		self.check_dir = "./output/"+ self.model + "/" + self.dataset +"/checkpoints"
-		self.images_dir = "./output/" + self.model + "/" + self.dataset + "/imgs"
+		self.tensorboard_dir = "./output/tensorboard"
+		self.check_dir = "./output/checkpoints"
 
 
 	def load_dataset(self):
@@ -31,37 +30,44 @@ class DD():
 		self.df['question1'] = self.df['question1'].apply(lambda x: str(x).strip())
 		self.df['question2'] = self.df['question2'].apply(lambda x: str(x).strip())
 
-
 		# Creating tokens for the given corpus
 
-		tk = tf.keras.preprocessing.text.Tokenizer(2000, lower="true", split=' ')
+		self.tk = tf.keras.preprocessing.text.Tokenizer(self.num_token, lower="true", split=' ')
 		
-		questions1 = self.df['question1'].values
-		questions2 = self.df['question2'].values
+		self.questions1 = self.df['question1'].values
+		self.questions2 = self.df['question2'].values
 		
-		tk.fit_on_texts(questions1 + questions2)
+		self.tk.fit_on_texts(self.questions1 + self.questions2)
 
-		self.question1_matrix = tk.texts_to_matrix(questions1, mode="tfidf")
-		self.question2_matrix = tk.texts_to_matrix(questions2, mode="tfidf")
-
-		self.word_index = tk.word_index
+		self.num_questions = self.questions1.size
+		self.word_index = self.tk.word_index
+		
 
 	def glove_emb(self):
 
 		self.glove_embedding = {}
+
+		self.n_words = len(self.word_index)
+
+		print("Loading glove matrix")
 		
-		f = open("data/glove.txt", "r")
+		f = open("data/glove.42B.300d.txt", "r")
 		for lines in f:
-			lines = lines.split(' ')
-			self.glove_embedding[lines[0]] = lines[1]
+			line = lines.split(' ')
+			self.glove_embedding[line[0]] = np.asarray(line[1:], dtype=np.float32)
 
-		n_words = len(self.word_index)
+		print("Done loading glove matrix")
 
-		self.embedding_matrix = np.zeros(n_words + 1, 300)
+		# sys.exit()
+
+		self.embedding_matrix = np.zeros((self.num_token, 300), dtype=np.float32)
 
 		for words in self.word_index.items():
-			if(words[0] in self.glove_embedding.keys()):
-				self.embedding_matrix[words[1]] = self.glove_embedding[words[0]]
+			temp = self.glove_embedding.get(words[0])
+			if(temp is not None and words[1] < self.num_token):
+				self.embedding_matrix[words[1]] = temp
+
+		print("Done making embedding matrix")
 
 
 
@@ -72,8 +78,9 @@ class DD():
 		o_l3 = linear1d(o_l2, 128, 128, name="l3")
 
 		o_conc = tf.concat([o_l1, o_l2, o_l3], axis=0)
+		out = tf.contrib.layers.batch_norm(o_enc, decay=0.9, updates_collections=None, epsilon=1e-5, scale=True, scope="batch_norm")
 
-		return o_conc
+		return out
 
 	def loss_setup(self, pred, act):
 
@@ -83,47 +90,86 @@ class DD():
 
 		with tf.variable_scope("Model") as scope:
 
-			qs1_ph = tf.placeholder(tf.float32, [self.batch_size, self.emb_size])
-			qs2_ph = tf.placeholder(tf.float32, [self.batch_size, self.emb_size])
-			y_ph = tf.placeholder(tf.float32, [self.batch_size, 1])
+			self.qs1_ph = tf.placeholder(tf.float32, [self.batch_size, self.emb_size])
+			self.qs2_ph = tf.placeholder(tf.float32, [self.batch_size, self.emb_size])
+			self.y_ph = tf.placeholder(tf.float32, [self.batch_size, 1])
 
-			o_net_1 = self.network(qs1_ph)
-			o_net_2 = self.network(qs2_ph)
+			o_net_1 = self.network(self.qs1_ph)
+			o_net_2 = self.network(self.qs2_ph)
 
 			pred_y = tf.sqrt(tf.mean(tf.squared_difference(o_net_1, o_net_2)))
 
-			self.loss = loss_setup(pred_y, 0)
+			self.loss = self.loss_setup(pred_y, self.y_ph)
 
 			optimizer = tf.train.AdamOptimizer(0.001, beta1=0.5)
 			self.loss_optimizer = optimizer.minimize(self.loss, var_list=enc_dec_vars)
 
 
-
-
 	def train(self):
 
-		# Loading dataset and creating embeddings
-		self.load_dataset()
 
-		sys.exit()
-		# Loading the glove embedding from the file
-		self.glove_emb()
+		if not os.path.exists(self.check_dir+"/glove1.npy"):
+
+			# Loading dataset and creating embeddings
+			self.load_dataset()
+
+			print("Done loading dataset")
+
+			# Loading the glove embedding from the file
+			self.glove_emb()
+
+			# print(self.embedding_matrix[1])
+
+			i = 0
+
+			self.question1_glove = np.zeros((self.num_questions, 300), dtype=np.float32)
+			self.question2_glove = np.zeros((self.num_questions, 300), dtype=np.float32)
+
+			# Making the glove vector by taking the sum of all glove vector of the words
+
+			while(i+1000 < self.num_questions):
+
+				temp = self.tk.texts_to_matrix(self.questions1[i:i+1000], mode="tqidf")
+				temp = np.matmul(temp, self.embedding_matrix)
+				self.question1_glove[i:i+1000] = temp
+
+				temp = self.tk.texts_to_matrix(self.questions2[i:i+1000], mode="tqidf")
+				temp = np.matmul(temp, self.embedding_matrix)
+				self.question2_glove[i:i+1000] = temp
+
+				i+=1000
+				print i
+
+			temp = self.tk.texts_to_matrix(self.questions1[i:], mode="tqidf")
+			temp = np.matmul(temp, self.embedding_matrix)
+			self.question1_glove[i:] = temp
+
+			temp = self.tk.texts_to_matrix(self.questions2[i:], mode="tqidf")
+			temp = np.matmul(temp, self.embedding_matrix)
+			self.question2_glove[i:] = temp
+
+			if not os.path.exists(self.check_dir):
+				os.makedirs(self.check_dir)
 
 
-		self.question1_glove = tf.matmul(self.question1_matrix, self.embedding_matrix)
-		self.question2_glove = tf.matmul(self.question2_matrix, self.embedding_matrix)
+			#Saving the vector representations for questions
+
+			np.save(self.check_dir+"/glove1.npy", self.question1_glove)
+			np.save(self.check_dir+"/glove2.npy", self.question2_glove)
+			self.y_train = self.df['is_duplicate'].values
+			np.save(self.check_dir+"/y_train.npy", self.y_train)
+
+		else :
+
+			self.question1_glove = np.load(self.check_dir+"/glove1.npy")
+			self.question2_glove = np.load(self.check_dir+"/glove2.npy")
+			self.y_train = np.load(self.check_dir+"/y_train.npy")
 
 		self.model_setup()
-		self.loss_setup()
 
 		init = tf.global_variables_initializer()
 		saver = tf.train.Saver()
-
-		if not os.path.exists(self.images_dir+"/train/"):
-			os.makedirs(self.images_dir+"/train/")
-		if not os.path.exists(self.check_dir):
-			os.makedirs(self.check_dir)
-
+		
 		with tf.Session() as sess:
 
 			sess.run(init)
@@ -132,13 +178,18 @@ class DD():
 			for epoch in range(0, self.max_epoch):
 				for itr in range(self.num_questions/self.batch_size):
 
-					feed_dict = { qs1_ph:self.question1_glove[itr*self.batch_size: (itr+1)*self.batch_size],
-								  qs2_ph:self.question2_glove[itr*self.batch_size: (itr+1)*self.batch_size]}
+					feed_dict = { self.qs1_ph:self.question1_glove[itr*self.batch_size: (itr+1)*self.batch_size],
+								  self.qs2_ph:self.question2_glove[itr*self.batch_size: (itr+1)*self.batch_size],
+								  self.y_ph:self.y_train[itr*self.batch_size: (itr+1)*self.batch_size]}
+
 					_ = sess.run(self.loss_optimizer, feed_dict=feed_dict)
+
+					sys.exit()
 
 def main():
 
 	model = DD()
+	model.initialize()
 	model.train()
 
 if __name__ == "__main__":
--- options.py
+++ options.py
@@ -7,21 +7,12 @@ class trainOptions():
 		self.initialized = False
 
 	def initialize(self):
-		self.parser.add_option('--num_iter', type='int', default=1000, dest='num_iter')
 		self.parser.add_option('--batch_size', type='int', default=100, dest='batch_size')
-		self.parser.add_option('--img_width', type='int', default=28, dest='img_width')
-		self.parser.add_option('--img_height', type='int', default=28, dest='img_height')
-		self.parser.add_option('--img_depth', type='int', default=1, dest='img_depth')
-		self.parser.add_option('--z_size', type='int', default=10, dest='z_size')
-		self.parser.add_option('--nef', type='int', default=16, dest='nef')
-		self.parser.add_option('--max_epoch', type='int', default=20, dest='max_epoch')
-		self.parser.add_option('--n_samples', type='int', default=50000, dest='n_samples')
+		self.parser.add_option('--max_epoch', type='int', default=50, dest='max_epoch')
+		self.parser.add_option('--num_token', type='int', default=20000, dest='num_token')
+		self.parser.add_option('--emb_size', type='int', default=300, dest='emb_size')
 		self.parser.add_option('--test', action="store_true", default=False, dest="test")
-		self.parser.add_option('--steps', type='int', default=10, dest='steps')
-		self.parser.add_option('--enc_size', type='int', default=256, dest='enc_size')
-		self.parser.add_option('--dec_size', type='int', default=256, dest='dec_size')
-		self.parser.add_option('--model', type='string', default="draw_attn", dest='model')
-		self.parser.add_option('--dataset', type='string', default="mnist", dest='dataset')
+		
 
 		self.initialized = True
 
